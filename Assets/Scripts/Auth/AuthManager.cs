d302ed16b6d4e3fd7b607a7831938fa8832b54fc
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-07-15 02:42:41 +0900

    add: AuthManager.cs

---
+++ Assets/Scripts/Auth/AuthManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using SocialGameClient.API;
+using System.IO.Pipes;
+using System.Reflection;
+
+namespace SocialGameClient.Auth
+{
+    public class AuthManager : MonoBehaviour
+    {
+        public static AuthManager Instance { get; private set; }
+
+        public string Token { get; private set; }
+        public long UserId { get; private set; }
+        public bool IsLoggendIn => !string.IsNullOrEmpty(Token);
+
+        private const string TokenPrefsKey = "auth_token";
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            // 発行済みトークン→読み込む
+            Token = PlayerPrefs.GetString(TokenPrefsKey, string.Empty);
+        }
+
+        public void Login(System.Action onSuccess, System.Action<string> onError)
+        {
+            string deviceId = SystemInfo.deviceUniqueIdentifier;
+
+            AnonymousPipeClientStream.Instance.Login(deviceId,
+                onSuccess: ResolveEventArgs =>
+                {
+                    Token = res.token;
+                    UserId = res.user_id;
+                    PlayerPrefs.SetString(TokenPrefsKey, Token);
+                    PlayerPrefs.Save();
+                    onSuccess?.Invoke();
+                },
+                onError: onError
+            );
+        }
+    }
+}
